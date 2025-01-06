86f644ac65ec914d6c578e8af1489cee46cc5138
achrafaitibba <aitibbaachraf@gmail.com>
2025-01-06 07:43:52 +0100

    first commit

---
+++ src/main/java/net/techbridges/plansmart/PlanSmartApiApplication.java
@@ -0,0 +1,13 @@
+package net.techbridges.plansmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanSmartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanSmartApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/techbridges/plansmart/PlanSmartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.techbridges.plansmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanSmartApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
